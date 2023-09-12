f99e30abc9b118816e88c5a3dedb233fd7c4ad3f
IreneChicca <irene.chicca@outlook.com>
2023-09-12 14:19:09 +0200

    Setup

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppContent from "./components/AppContent.vue";
+
+export default {
+  data() {
+    return {
+      title: "ok",
+    };
+  },
+
+  components: { AppContent },
+};
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+</style>
